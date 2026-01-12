6c59977b510116f8d3737b459995c4a714eef2db
Anderson092205 <andersonalfredoescobarmoran@gmail.com>
2026-01-12 11:25:37 -0600

    Subiendo el backend

---
+++ src/main/java/sv/gob/cementerios/cementeriole/CementerioLeApplication.java
@@ -0,0 +1,13 @@
+package sv.gob.cementerios.cementeriole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CementerioLeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CementerioLeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sv/gob/cementerios/cementeriole/CementerioLeApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.gob.cementerios.cementeriole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CementerioLeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
